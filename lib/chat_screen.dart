ef752ecbbbe5c1129eeb76fb67b91c075e12d416
Osama0habib <86162362+Osama0habib@users.noreply.github.com>
2022-11-20 22:00:16 +0200

    FireStore

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -5,6 +6,43 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection("chat")
+              .snapshots()
+              .listen((event) {
+            event.docs.forEach((element) {
+              var data = element.data();
+              print(data["msgBody"]);
+            });
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("chat").snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          List<Map<String,dynamic>> data = [];
+          snapshot.data?.docs.forEach((element) {
+            data.add(element.data());
+
+          });
+
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(data[index]["msgBody"]),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
